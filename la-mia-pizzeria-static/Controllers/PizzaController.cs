a1ae8a4d2d52614e64d5147bba63376df878c09a
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-09-26 16:49:08 +0200

    feat: other features added

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using la_mia_pizzeria_static.Models;  
+using la_mia_pizzeria_static.Models;
 using System.Collections.Generic;
 using Bogus;
 
@@ -11,11 +11,11 @@ namespace la_mia_pizzeria_static.Controllers
         public IActionResult Index()
         {
             // Generate a list of fake pizzas
-        var faker = new Faker<Pizza>()
-            .RuleFor(p => p.Id, f => f.Random.Int(1, 100))
-            .RuleFor(p => p.Name, f => f.Random.Word())
-            .RuleFor(p => p.Description, f => f.Lorem.Sentence())
-            .RuleFor(p => p.Price, f => (Decimal)f.Random.Double(5, 20));
+            var faker = new Faker<Pizza>()
+                .RuleFor(p => p.Id, f => f.Random.Int(1, 100))
+                .RuleFor(p => p.Name, f => f.Random.Word())
+                .RuleFor(p => p.Description, f => f.Lorem.Sentence())
+                .RuleFor(p => p.Price, f => Math.Round((Decimal)f.Random.Double(5, 20), 2));
 
             List<Pizza> pizzas = faker.Generate(10);  // Generate 10 fake pizzas
 
